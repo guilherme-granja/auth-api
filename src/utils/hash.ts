3c3c0bee04c3868c73219ec5bdc17f8ca264dc9c
guilherme-granja <guilherme.marcal@soluti.com.br>
2026-02-02 18:16:24 +0000

    feat: add HashUtils class for password hashing and comparison

---
+++ src/utils/hash.ts
@@ -0,0 +1,24 @@
+import bcrypt from 'bcrypt';
+
+export class HashUtils {
+    private static readonly ROUNDS = parseInt(process.env.BCRYPT_ROUNDS || '10', 10);
+
+    static async hash(password: string): Promise<string> {
+        return await bcrypt.hash(
+            password,
+            this.ROUNDS
+        );
+    }
+
+    static async compare(password: string, hashedPassword: string): Promise<boolean> {
+        return await bcrypt.compare(
+            password,
+            hashedPassword
+        );
+    }
+
+    static async needsRehash(hashedPassword: string) {
+        const round: number = bcrypt.getRounds(hashedPassword);
+        return round !== this.ROUNDS;
+    }
+}
