423bcd7f7a2c6f51e207f08909c342e4d8a75e8f
guilherme-granja <guilherme.marcal@soluti.com.br>
2026-02-04 17:30:58 +0000

    refactor: format import statements for consistency in authRoutes

--- src/routes/api/auth/authRoutes.ts
+++ src/routes/api/auth/authRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import {AuthController} from "../../../controllers/AuthController";
-import {validateRequest} from "../../../middlewares/validateRequest";
-import {registerSchema} from "../../../validators/authSchemas";
+import { AuthController } from "../../../controllers/AuthController";
+import { validateRequest } from "../../../middlewares/validateRequest";
+import { registerSchema } from "../../../validators/authSchemas";
 
 const router = Router();
 const authController = new AuthController();
